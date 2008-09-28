fe69614fecb72dba4b345ca25e939a7b62e28a26
Y Giridhar Appaji Nag <giridhar@appaji.net>
2008-09-28 19:00:19 +0530

    Imported Upstream version 0.12~pre2

--- src/scripting/smjs/core.h
+++ src/scripting/smjs/core.h
@@ -1,7 +1,7 @@
 #ifndef EL__SCRIPTING_SMJS_CORE_H
 #define EL__SCRIPTING_SMJS_CORE_H
 
-#include "ecmascript/spidermonkey/util.h"
+#include "ecmascript/spidermonkey-shared.h"
 
 struct module;
 struct session;
@@ -10,6 +10,7 @@ struct string;
 extern JSContext *smjs_ctx;
 extern struct session *smjs_ses;
 
+void smjs_error_reporter(JSContext *ctx, const char *message, JSErrorReport *report);
 void alert_smjs_error(unsigned char *msg);
 
 void init_smjs(struct module *module);
